a76868fc3e3fa3226733939fe31a40b881c7c923
SSkedge <19799750+SSkedge@users.noreply.github.com>
2020-07-06 20:15:55 +0100

    Added table and code blocks to markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,96 +1,44 @@
-import React from "react"
-import { createGlobalStyle, ThemeProvider } from "styled-components";
-import Theme from "./src/themes/theme";
-import { BackgroundGradient, GradientBar } from "./src/components";
-import { preToCodeBlock } from "mdx-utils";
-import { MDXProvider } from "@mdx-js/react";
-import { Code } from "./src/components"
+exports.createPages = async function ({ actions, graphql }) {
+    const { data } = await graphql(`
+     query {
+         allMdx(sort: { fields: frontmatter___date, order: DESC}){
+            edges {
+                node {
+                    frontmatter {
+                        slug
+                        
+                    }
+                    id
+                }
+            }
+         }
+     }
+    `)
 
-const GlobalStyles = createGlobalStyle`
-    * {
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
+    const postsPerPage = 3;
 
-    body, html {
-        font-family: ${props => props.theme.fonts.main};
-        height: 100%;
-        background-color: ${props => props.theme.colours.light1};
-    }
+    const numPages = Math.ceil(data.allMdx.edges.length / postsPerPage);
 
-    .gatsby-highlight {
-        position: relative;
-        -webkit-overflow-scrolling: touch;
-    }
-    
-    .gatsby-highlight:not(:last-child) {
-        margin-top: 2rem;
-    }
-    
-    .gatsby-highlight pre[class*="language-"] {
-        -webkit-overflow-scrolling: touch;
-        padding: 1rem 0.75rem;
-        border-radius: 0.25rem;
-    }
-    .gatsby-highlight pre[class*="language-"]::before {
-        background: black;
-        border-radius: 0 0 0.25rem 0.25rem;
-        color: white;
-        font-size: 12px;
-        letter-spacing: 0.025rem;
-        padding: 0.1rem 0.5rem;
-        position: absolute;
-        right: 1rem;
-        text-align: right;
-        text-transform: uppercase;
-        top: 0;
-    }
-    .gatsby-highlight pre[class~="language-javascript"]::before {
-        content: "js";
-        background: #f7df1e;
-        color: black;
-    }
-    .gatsby-highlight pre[class~="language-js"]::before {
-        content: "js";
-        background: #f7df1e;
-        color: black;
-    }
-    
-    .gatsby-highlight pre[class~="language-html"]::before {
-        content: "html";
-        background: #005a9c;
-        color: white;
-    }
-    
-    .gatsby-highlight pre[class~="language-css"]::before {
-        content: "css";
-        background: #ff9800;
-        color: white;
-    }
-  
-`
+    Array.from({ length: numPages }).forEach((_, i) => {
+        actions.createPage({
+            path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+            component: require.resolve("./src/templates/allPosts.js"),
+            context: {
+                limit: postsPerPage,
+                skip: i * postsPerPage,
+                numPages,
+                currentPage: i + 1,
+            }
+        });
+    });
 
-const components = {
-    pre: preProps => {
-        const props = preToCodeBlock(preProps)
-        // if there's a codeString and some props, we passed the test
-        if (props) {
-            return <Code {...props} />
-        }
-        // it's possible to have a pre without a code in it
-        return <pre {...preProps} />
-    },
-    wrapper: ({ children }) => <>{children}</>,
+    data.allMdx.edges.forEach(edge => {
+        const slug = edge.node.frontmatter.slug;
+        const id = edge.node.id;
+        actions.createPage({
+            path: `/blog/${slug}`,
+            component: require.resolve(`./src/templates/singlePost.js`),
+            context: { id },
+        });
+    });
 }
-
-export const wrapRootElement = ({ element }) => (
-    <MDXProvider components={components}>
-        <ThemeProvider theme={Theme}>
-            <GlobalStyles />
-            <BackgroundGradient />
-            <GradientBar />
-            {element}
-        </ThemeProvider>
-    </MDXProvider>
-)
